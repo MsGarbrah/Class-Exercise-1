e360f3b3cb0d4526ce133a95d773a6c5b5ef9a8b
Christabelle <christefani@yahoo.com>
2023-06-10 14:58:43 +0000

    add objects.js

---
+++ objects.js
@@ -0,0 +1,27 @@
+const person = {
+    name: 'Yvonne',
+    email: 'yvonne@example.com',
+    age: 27 
+  };
+  
+
+  console.log(person);
+  
+  let teacher = {
+    name: 'Michelle',
+    school: 'Ace School',
+    subject: 'English',
+    class: 6
+  };
+  
+  console.log(teacher);
+
+
+  let car = {
+    brand: 'Toyota',
+    model: 'Land Cruiser',
+    year: 2019,
+    color: 'black'   
+  }
+
+  console.log(car);
